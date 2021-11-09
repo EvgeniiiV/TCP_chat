abe4ca66bfeca17187f8d825462ed1f139a1be2c
EvgeniiiV <84666289+EvgeniiiV@users.noreply.github.com>
2021-11-09 22:42:24 +0500

    Update server_chat.h

--- server_chat.h
+++ server_chat.h
@@ -2,14 +2,10 @@
 #include<iostream>
 using namespace std;
 #include<string>
+#include<vector>
+#define MESSAGE_LENGTH 1024 // Максимальный размер буфера для данных
+char ch_message[MESSAGE_LENGTH];
 
-
-template <typename T> bool compare_LP(T LP, T L, T P);//compares login and password
-template <typename T1, typename T2> T2 request(T1 rst, T2 a, T2 b, T2 first);//for registrarion
-template <typename T> T next_request();//case 5, 6
-inline size_t Factorial(size_t num);//factorial
-inline size_t Presence(size_t num);//to calculate max number of possible groups 
-//for one user based on the number of registered users
-
-void my_send( string mes);
-string my_receive ();
+template <typename T> bool compare_LP(T LP, T L, T P);
+void my_send( string mes, int connection);
+string my_receive (int connection);
