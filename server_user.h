d9281b366780af1e05fad5cc18b96a311f3e4091
EvgeniiiV <84666289+EvgeniiiV@users.noreply.github.com>
2021-11-09 22:46:27 +0500

    Update server_user.h

--- server_user.h
+++ server_user.h
@@ -1,13 +1,18 @@
 #pragma once
 
+
 template <typename T>class User
 {
 private:
-    T _name, _login, _pass;
-public:
+    int _connection;
+    T _name, _login, _pass;    
+        
+public:    
     User();
-    User(T name, T login, T pass);
+    User(T name, T login, T pass, int connection);
     T get_name() const;
     T get_log_pas() const;
+    int get_FD_by_name() const {return _connection;}
+    T get_name_by_FD() const {return _name;}
 };
 
