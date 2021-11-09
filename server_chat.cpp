20c741d35955b24e0a0d3332280613233ed07c58
EvgeniiiV <84666289+EvgeniiiV@users.noreply.github.com>
2021-11-09 22:41:18 +0500

    Update server_chat.cpp

--- server_chat.cpp
+++ server_chat.cpp
@@ -6,7 +6,8 @@ using namespace std;
 #include <streambuf>
 #include "server_chat.h"
 #include <vector>
-
+#include <sys/socket.h>
+#define MESSAGE_LENGTH 1024 // Максимальный размер буфера для данных
 
 
 
@@ -15,73 +16,31 @@ template <typename T> bool compare_LP(T LP, T L, T P)//compares login and passwo
     return LP == L + P;
 }
 
-template <typename T1, typename T2> T2 request(T1 rst, T2 a, T2 b, T2 first)
-{
-    stringstream s_s;
-    T2 choice = 0;
-    if (!first)
+//server -> client
+void my_send(string _mes, int connection)
+{   
+    ssize_t bytes;  
+    bzero(ch_message, MESSAGE_LENGTH);
+    strncpy (ch_message, _mes.c_str(), MESSAGE_LENGTH);
+    if (_mes.size() >= MESSAGE_LENGTH)
     {
-        do//to prevent invalid input
-        {   my_send (s_s.str() + "REGISTRATION of next USER: press key 1\n");      
-            s_s.str("");       
-            string rcv =  my_receive();//receive from client
-            choice = (size_t)stoi (rcv);       
-            if (choice != a)            
-            s_s << "Enter a proper digit\n";
-        } 
-        while (choice != a);
-    }
-    if (first)
-    {
-        do//to prevent invalid input during 
-        {            
-            my_send(s_s.str() + rst);
-            s_s.str("");           
-            string rcv =  my_receive();
-            choice = (size_t)stoi (rcv);            
-            if (choice != a && choice != b)          
-            s_s << "Enter proper digit\n";   
-        } 
-        while (choice != a && choice != b);
-    }
-    return choice;
-}
-
-template <typename T> T next_request()//case 5, 6
+        ch_message[MESSAGE_LENGTH-1] = '\0';
+    }               
+    bytes = send(connection, ch_message, sizeof(ch_message), 0);
+    if (bytes < 0) cout <<  "send failed" << endl;
+       
+}  
+ 
+//client -> server
+string my_receive (int connection)
 {    
-    size_t choice = 0;
-    do
-    {        
-        my_send ("\nLogout and Registration: press key 1\nLogout and Authorization: press key 2\nCreate new contact: press key 3\nChose another contact: press key 4\nNext message to this contact: press key  5\nMessage for everyone: press key 6\nQuit: press key 7\n");
-        string rcv = my_receive();
-        choice = (size_t)stoi (rcv);        
-        if (choice < 1 || choice > 7)           
-        my_send( "ENTER 1 - 7\n");
-    } while (choice < 1 || choice > 7);
-    return choice;
-}
-
-size_t Factorial(size_t num)
-{
-    size_t p = 0;
-    size_t f = 1;
-    for (size_t i = 1; i <= num; i++)
-        f *= i;
-    return (num != 0) ? f : 1;
-}
-
-size_t Presence(size_t num)
-{
-    if (num <= 2) return 1;
-    num--;
-    size_t p = 0;
-    size_t d = num;
-    do
+    bzero(ch_message, MESSAGE_LENGTH);
+    ssize_t bytes = recv(connection, ch_message, MESSAGE_LENGTH, 0);    
+    if (bytes < 0) 
     {
-        p += Factorial(num) / (Factorial(num - d) * Factorial(d));
-        d--;
-    } while (d >= 2);
-    p += num;
-
-    return p;
-}
+        perror("recv");        
+    }   
+    string s_read = string (ch_message, strlen(ch_message));//for received msg    
+    cout << s_read << endl;  
+    return s_read;    
+}  
