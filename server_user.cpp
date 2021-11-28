b8e82b35938c21b8dcce977372231555ee61d6df
EvgeniiiV <84666289+EvgeniiiV@users.noreply.github.com>
2021-11-28 13:56:06 +0500

    Update server_user.cpp

--- server_user.cpp
+++ server_user.cpp
@@ -2,6 +2,6 @@
 
 template<typename T>User<T>::User() = default;
 template<typename T>User<T>::User
-(T name, T login, T pass, int connection) : _name(name), _login(login), _pass(pass), _connection(connection) {}
+(T name, T login, T pass) : _name(name), _login(login), _pass(pass){}
 template<typename T> T User<T>::get_name() const { return _name; }
 template<typename T> T User<T>::get_log_pas() const { return _login + _pass; }
