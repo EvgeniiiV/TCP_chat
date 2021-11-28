9feef40a75a6a8b18093049404867d06b9fd4c9d
EvgeniiiV <84666289+EvgeniiiV@users.noreply.github.com>
2021-11-28 13:33:23 +0500

    Update server_user.h

--- server_user.h
+++ server_user.h
@@ -4,15 +4,14 @@
 template <typename T>class User
 {
 private:
-    int _connection;
+    
     T _name, _login, _pass;    
         
 public:    
     User();
-    User(T name, T login, T pass, int connection);
+    User(T name, T login, T pass);
     T get_name() const;
     T get_log_pas() const;
-    int get_FD_by_name() const {return _connection;}
-    T get_name_by_FD() const {return _name;}
+   
 };
 
